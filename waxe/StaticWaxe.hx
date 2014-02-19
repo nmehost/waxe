fb8469f22d4a7df44250721786f3736a6d037bd3
Hugh <gamehaxe@gmail.com>
2014-02-20 00:41:17 +0800

    Allow for msvc linking with versions other than 18

--- waxe/StaticWaxe.hx
+++ waxe/StaticWaxe.hx
@@ -3,8 +3,8 @@ package waxe;
 @:cppFileCode( 'extern "C" void waxe_register_prims();')
 @:buildXml("
 <target id='haxe'>
-  <lib name='${haxelib:waxe}/lib/${BINDIR}/libwaxe${LIBEXTRA}${LIBEXT}'/>
-  <lib name='${haxelib:waxe-works}/lib/${BINDIR}/libwxwidgets${LIBEXTRA}${LIBEXT}'/>
+  <lib name='${haxelib:waxe}/lib/${BINDIR}/libwaxe{MSVC_VER}${LIBEXTRA}${LIBEXT}'/>
+  <lib name='${haxelib:waxe-works}/lib/${BINDIR}/libwxwidgets{MSVC_VER}${LIBEXTRA}${LIBEXT}'/>
 </target>
 <include name='${haxelib:waxe}/lib/WaxeLink.xml'/>
 ")
