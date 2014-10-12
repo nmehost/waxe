7a7609548b0afa4068df9dae4434cf9945c19585
Jonas Nystrom <jonasnys@gmail.com>
2014-10-12 09:54:09 +0200

    Added button to 05-EventHandlers

--- samples/05-EventHandlers/Main.hx
+++ samples/05-EventHandlers/Main.hx
@@ -4,33 +4,35 @@ import wx.App;
 import wx.StaticText;
 import wx.TextCtrl;
 import wx.Frame;
+import wx.Button;
 //import wx.MessageDialog;
 
 class Main
 {
-	var frame : Frame;
-	var textCtrl:TextCtrl;
-	var staticText:StaticText;
-	var counter:Int = 1;
-
+	var counter:Int = 0;
 	function new()
 	{
-		this.frame = wx.Frame.create(null, null, "This is a Frame", null, { width: 800, height: 600 });
-		this.textCtrl = TextCtrl.create(frame, null, 'Type something here', {x:20, y:40}, {width:200, height:22});
-		this.staticText = StaticText.create(frame, null, 'StaticText', { x:20, y: 80 } );
+		var frame : Frame = Frame.create(null, null, "This is a Frame", null, { width: 800, height: 600 });
+		var textCtrl:TextCtrl = TextCtrl.create(frame, null, 'Type something here', {x:20, y:40}, {width:200, height:22});
+		var staticText:StaticText = StaticText.create(frame, null, 'StaticText', { x:20, y: 80 } );
+		var btnClose:Button = Button.create(frame,null,"Close",  { x:20, y: 120 } );
 		
-		this.textCtrl.onTextUpdated = function(e) {
+		textCtrl.onTextUpdated = function(e) {
 			staticText.label = textCtrl.value;
 		}
 		
-		this.frame.onSize = function(e) { 
-			this.staticText.label = 'Frame size is changed : $counter';
+		btnClose.onClick = function(e) {
+			App.quit();
+		}		
+		
+		frame.onSize = function(e) { 
+			staticText.label = 'Frame size is changed : $counter';
 			counter++;
 			e.skip = true; 
 		}		
 
-		App.setTopWindow(this.frame);
-		this.frame.shown = true;
+		App.setTopWindow(frame);
+		frame.shown = true;
 	}
 
 	public static function main()
